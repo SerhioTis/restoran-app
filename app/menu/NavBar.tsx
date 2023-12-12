35ca6aa38beb85eaf740420dcf621ab3ce17d80c
OlehDaschuk <o.v.dashchuk.fitis21@chdtu.edu.ua>
2023-12-12 20:43:34 +0200

    docs: added user and session tabels

--- app/menu/NavBar.tsx
+++ app/menu/NavBar.tsx
@@ -3,28 +3,24 @@ import { Fragment } from 'react';
 
 import Link from 'next/link';
 
-import { Input } from 'components/ui/input';
-
-import { useSearchProduct } from './store';
-
 interface NavBarProps {
   productSubTypes: Record<string, string[]>;
 }
 
 export const NavBar = ({ productSubTypes }: NavBarProps) => {
-  const { search, setSearch } = useSearchProduct();
+  // const { search, setSearch } = useSearchProduct();
   const productSubtypesList = Object.keys(productSubTypes);
 
   return (
     <div className="h-full pt-7">
-      <div className="mx-4 mb-6">
+      {/* <div className="mx-4 mb-6">
         <Input
           type="search"
           placeholder="Назва страви"
           value={search}
           onChange={(event) => setSearch(event.target.value)}
         />
-      </div>
+      </div> */}
       <ul className="pb-4">
         {productSubtypesList.map((type) => (
           <Fragment key={type}>
--- database/database.sql
+++ database/database.sql
@@ -8,3 +8,21 @@ CREATE TABLE IF NOT EXISTS products  (
   sub_type TEXT NOT NULL,
   price INTEGER NOT NULL
 );
+
+CREATE TYPE USER_TYPE AS ENUM ('user', 'courier', 'admin');
+
+CREATE TABLE IF NOT EXISTS users  (
+  id SERIAL PRIMARY KEY,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL,
+  phone TEXT NOT NULL,
+  type USER_TYPE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_session  (
+  id SERIAL PRIMARY KEY,
+  access_token TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()::TIMESTAMP,
+  user_id INTEGER
+);
+
